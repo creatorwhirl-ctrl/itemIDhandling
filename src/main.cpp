09a47bc0a1648c3be1eb352c5a1bd93b376094e2
creatorwhirl-ctrl <creatorwhirl@gmail.com>
2026-07-23 00:53:57 +0200

    attempt5

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,27 @@
 #include <Geode/Geode.hpp>
-using namespace geode::prelude;
-
 #include <Geode/modify/PlayLayer.hpp>
 
+using namespace geode::prelude;
+
 class $modify(MyPlayLayer, PlayLayer) {
-    void keyDown(cocos2d::enumKeyCodes key, double timestamp) {
-        if (key == cocos2d::enumKeyCodes::KEY_U) {
 
-            // 1. Show alert popup on screen
-            FLAlertLayer::create(
-                "Item Modified!",           // Title
-                "Item ID 1 count set to 5", // Message body
-                "OK"                        // Button text
-            )->show();
+	void keyDown(cocos2d::enumKeyCodes key) {
+		if (key == cocos2d::enumKeyCodes::KEY_U) {
 
-            // 2. Set item count directly in the effect manager map
-            if (this->m_effectManager) {
-                // Set Item ID 1 count to 5 in the m_itemValues map
-                this->m_effectManager->m_itemValues[1] = 5;
+			log::debug("U pressed. m_effectManager = {}", fmt::ptr(this->m_effectManager));
 
-                // Read back using countForItem (confirmed in bindings)
-                int currentCount = this->m_effectManager->countForItem(1);
-                log::info("Successfully updated Item ID 1 to: {}", currentCount);
-            }
-        }
+			if (this->m_effectManager) {
+				int itemID = 1;
+				int newValue = 10;
 
-        // Call regular base key event so controls keep working
-        PlayLayer::keyDown(key, timestamp);
-    }
+				int beforeValue = this->m_effectManager->countForItem(itemID);
+				this->m_effectManager->updateCountForItem(itemID, newValue);
+				int afterValue = this->m_effectManager->countForItem(itemID);
+
+				log::debug("Item {}: {} -> {}", itemID, beforeValue, afterValue);
+			}
+		}
+
+		PlayLayer::keyDown(key);
+	}
 };
