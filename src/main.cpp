6da25250cc70cb8370109ecec82a1ee8540f276f
creatorwhirl-ctrl <creatorwhirl@gmail.com>
2026-07-23 00:26:52 +0200

    when

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,21 @@
 using namespace geode::prelude;
 
 #include <Geode/modify/PlayLayer.hpp>
+
 class $modify(MyPlayLayer, PlayLayer) {
-	void keyDown(cocos2d::enumKeyCodes key) {
-		if (key == cocos2d::enumKeyCodes::KEY_U) {
-			log::debug("U pressed, m_effectManager is {}", fmt::ptr(this->m_effectManager));
+    // 1. Add the missing `double timestamp` parameter
+    void keyDown(cocos2d::enumKeyCodes key, double timestamp) {
+        if (key == cocos2d::enumKeyCodes::KEY_U) {
+            log::debug("U pressed, m_effectManager is {}", fmt::ptr(this->m_effectManager));
 
-			if (this->m_effectManager) {
-				// this->m_effectManager->countForItem(1);
-			}
-		}
+            if (this->m_effectManager) {
+                // countForItem returns the current item/item-ID count
+                int count = this->m_effectManager->countForItem(1);
+                log::debug("Item ID 1 count: {}", count);
+            }
+        }
 
-		PlayLayer::keyDown(key);
-	}
+        // 2. Pass both arguments to the base function call
+        PlayLayer::keyDown(key, timestamp);
+    }
 };
