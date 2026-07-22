0d44de4c9d2bcf2a0abc0edcb5976b93bdbbfa83
creatorwhirl-ctrl <creatorwhirl@gmail.com>
2026-07-22 23:59:20 +0200

    Test2

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,14 @@
 using namespace geode::prelude;
 
 class $modify(MyPlayLayer, PlayLayer) {
-    void keyDown(cocos2d::enumKeyCodes key) {
-        // Check if player pressed 'U' while in a level
-        if (key == cocos2d::enumKeyCodes::KEY_U) {
+    void keyDown(enumKeyCodes key) {
+        if (key == enumKeyCodes::KEY_U) {
             if (this->m_effectManager) {
-                // Set Item ID 1 to value 13 immediately
                 this->m_effectManager->setItemCount(1, 13, false, 0);
-                
-                // Show a quick notification on screen
                 Notification::create("Set Item ID 1 to 13!", NotificationIcon::Success)->show();
             }
         }
 
-        // Always call the original keyDown so normal controls still work
         PlayLayer::keyDown(key);
     }
 };
