93e82d2ed61166f57043076ba6e8ac4170a7eb0c
creatorwhirl-ctrl <creatorwhirl@gmail.com>
2026-08-01 01:00:52 +0200

    test11

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,60 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/PlayLayer.hpp>
 #include <Geode/utils/Keyboard.hpp>
 #include <Geode/ui/Notification.hpp>
+#include <array>
 
 using namespace geode::prelude;
+
+namespace {
+    constexpr int kFirstItemID = 1;
+    constexpr int kItemCount = 16;
+    std::array<int, kItemCount> g_itemClipboard{};
+    bool g_hasCopied = false;
+}
 
 $execute {
     KeyboardInputEvent()
         .listen(+[](const geode::KeyboardInputData& event) {
             if (event.action != KeyboardInputData::Action::Press) return ListenerResult::Propagate;
-            if (event.key != cocos2d::enumKeyCodes::KEY_Q) return ListenerResult::Propagate;
 
             auto pl = PlayLayer::get();
-            if (!pl || !pl->m_effectManager) {
-                Notification::create("No active PlayLayer / effect manager", NotificationIcon::Error, 1.5f)->show();
+
+            // Q: set item 1 to 16 (your existing test key)
+            if (event.key == cocos2d::enumKeyCodes::KEY_Q) {
+                if (!pl || !pl->m_effectManager) {
+                    Notification::create("No active PlayLayer / effect manager", NotificationIcon::Error, 1.5f)->show();
+                    return ListenerResult::Propagate;
+                }
+                pl->m_effectManager->updateCountForItem(1, 16);
+                pl->updateCounters(1, 16);
+                Notification::create("Item 1 set to 16", NotificationIcon::Success, 1.0f)->show();
                 return ListenerResult::Propagate;
             }
 
-            int itemID = 1;
-            int newValue = 16;
-            int before = pl->m_effectManager->countForItem(itemID);
-            pl->m_effectManager->updateCountForItem(itemID, newValue);
-            pl->updateCounters(itemID, newValue);
-            int after = pl->m_effectManager->countForItem(itemID);
-
-            log::debug("Item {}: {} -> {}", itemID, before, after);
-            Notification::create(fmt::format("Item {} set to {}", itemID, after), NotificationIcon::Success, 1.0f)->show();
+            // Ctrl+C / Ctrl+V: copy-paste item IDs 1-16
+            if (event.modifiers & KeyboardModifier::Control) {
+                if (event.key == cocos2d::enumKeyCodes::KEY_C) {
+                    if (!pl || !pl->m_effectManager) return ListenerResult::Propagate;
+                    for (int i = 0; i < kItemCount; i++) {
+                        g_itemClipboard[i] = pl->m_effectManager->countForItem(kFirstItemID + i);
+                    }
+                    g_hasCopied = true;
+                    Notification::create(fmt::format("Copied items {}-{}", kFirstItemID, kFirstItemID + kItemCount - 1), NotificationIcon::Success, 1.0f)->show();
+                }
+                else if (event.key == cocos2d::enumKeyCodes::KEY_V) {
+                    if (!pl || !pl->m_effectManager) return ListenerResult::Propagate;
+                    if (!g_hasCopied) {
+                        Notification::create("Nothing copied yet", NotificationIcon::Error, 1.0f)->show();
+                        return ListenerResult::Propagate;
+                    }
+                    for (int i = 0; i < kItemCount; i++) {
+                        int id = kFirstItemID + i;
+                        pl->m_effectManager->updateCountForItem(id, g_itemClipboard[i]);
+                        pl->updateCounters(id, g_itemClipboard[i]);
+                    }
+                    Notification::create(fmt::format("Pasted items {}-{}", kFirstItemID, kFirstItemID + kItemCount - 1), NotificationIcon::Success, 1.0f)->show();
+                }
+            }
 
             return ListenerResult::Propagate;
         }, -100)
