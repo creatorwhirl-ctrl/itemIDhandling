8a93bb6695ba57f2806abfdc8c78848f24fbc40c
creatorwhirl-ctrl <creatorwhirl@gmail.com>
2026-08-01 00:39:24 +0200

    test9

--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,19 @@
 using namespace geode::prelude;
 
 class $modify(MyPlayLayer, PlayLayer) {
-
 	void keyDown(cocos2d::enumKeyCodes key, double dt) {
-
-		// DIAGNOSTIC: logs every single key press so we can confirm the
-		// hook is firing at all, regardless of which key is pressed.
-		log::debug("keyDown fired, key = {}", (int)key);
-
-		if (key == cocos2d::enumKeyCodes::KEY_J) {
-
-			// Sanity check: confirms the hook fired and the effect manager
-			// is valid before we try to touch it. Check this first in the
-			// in-game console (Geode Settings -> Console) if item ID 1
-			// still doesn't change after pressing J.
-			log::debug("J pressed. m_effectManager = {}", fmt::ptr(this->m_effectManager));
-
+		if (key == cocos2d::enumKeyCodes::KEY_Q) {
 			if (this->m_effectManager) {
 				int itemID = 1;
-				int newValue = 10;
-
+				int newValue = 16;
 				int beforeValue = this->m_effectManager->countForItem(itemID);
 				this->m_effectManager->updateCountForItem(itemID, newValue);
+				this->updateCounters(itemID, newValue); // refreshes any on-screen item label bound to this ID
 				int afterValue = this->m_effectManager->countForItem(itemID);
-
 				log::debug("Item {}: {} -> {}", itemID, beforeValue, afterValue);
 			}
 		}
 
-		// Always forward to the original so other keybinds keep working.
 		PlayLayer::keyDown(key, dt);
 	}
 };
